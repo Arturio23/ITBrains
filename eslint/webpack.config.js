1985c4242e936e9373019863d4ae16e2511ce68e
imag <daysartur@gmail.com>
2023-03-01 00:15:16 +0200

    eslint and prettier

--- eslint/webpack.config.js
+++ eslint/webpack.config.js
@@ -1,25 +1,25 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './resources/index.js',
+  entry: "./resources/index.js",
   output: {
-    path: path.join(__dirname, '/public/dist'),
-    publicPath: '/public/dist',
-    filename: 'app.js',
+    path: path.join(__dirname, "/public/dist"),
+    publicPath: "/public/dist",
+    filename: "app.js",
   },
-  mode: 'development',
+  mode: "development",
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
         },
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
