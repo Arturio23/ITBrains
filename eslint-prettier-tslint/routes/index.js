525bcf22d0882d26b5b7857b48715aadf0d4a971
imag <daysartur@gmail.com>
2023-03-02 02:55:03 +0200

    add newVersiom

--- eslint-prettier-tslint/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const hbs = require("hbs");
-
-router.get("/", function (req, res, next) {
-  res.render("template", { name: "Hello world!" });
-});
-
-module.exports = router;
