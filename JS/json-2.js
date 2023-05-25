99237179a9cd31d554d5c7f00df015db6cb05fc9
imag <daysartur@gmail.com>
2023-05-25 15:10:39 +0300

    sss

---
+++ JS/json-2.js
@@ -0,0 +1,14 @@
+let room = {
+  number: 23
+};
+
+let meetup = {
+  title: "Конференція",
+  participants: [{name: "Іван"}, {name: "Аліна"}],
+  place: room // meetup посилається на room
+};
+
+room.occupiedBy = meetup; // room посилається на meetup
+
+alert( JSON.stringify(meetup, ['title', 'participants']) );
+// {"title":"Конференція","participants":[{},{}]}
