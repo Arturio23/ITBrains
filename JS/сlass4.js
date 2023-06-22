996c8625b1c0239fb1e3935f932c713f105165ad
imag <daysartur@gmail.com>
2023-06-22 13:14:38 +0300

    ss

---
+++ JS/сlass4.js
@@ -0,0 +1,21 @@
+class Animal {
+  constructor(name) {
+    this.speed = 0;
+    this.name = name;
+  }
+  // ...
+}
+
+class Rabbit extends Animal {
+
+  constructor(name, earLength) {
+    this.speed = 0;
+    this.name = name;
+    this.earLength = earLength;
+  }
+
+  // ...
+}
+
+// Не працює!
+let rabbit = new Rabbit("Білий Кролик", 10); // Error: this is not defined.
