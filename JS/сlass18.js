007d3c42b00cc1cb6f1adba50fdcfe751c22e40f
imag <daysartur@gmail.com>
2023-06-22 15:25:48 +0300

    ss

---
+++ JS/сlass18.js
@@ -0,0 +1,13 @@
+// додамо до нього ще один метод (можна більше)
+class PowerArray extends Array {
+  isEmpty() {
+    return this.length === 0;
+  }
+}
+
+let arr = new PowerArray(1, 2, 5, 10, 50);
+alert(arr.isEmpty()); // false
+
+let filteredArr = arr.filter(item => item >= 10);
+alert(filteredArr); // 10, 50
+alert(filteredArr.isEmpty()); // false
