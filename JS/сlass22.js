e094924ec1dcf40964283eb538a911a56b4e3269
imag <daysartur@gmail.com>
2023-06-28 14:20:16 +0300

    ss

---
+++ JS/сlass22.js
@@ -0,0 +1,40 @@
+let eventMixin = {
+  /**
+   * Підписатися на подію, використання:
+   *  menu.on('select', function(item) { ... }
+   */
+  on(eventName, handler) {
+    if (!this._eventHandlers) this._eventHandlers = {};
+    if (!this._eventHandlers[eventName]) {
+      this._eventHandlers[eventName] = [];
+    }
+    this._eventHandlers[eventName].push(handler);
+  },
+
+  /**
+   * Скасувати підписку, використання:
+   *  menu.off('select', handler)
+   */
+  off(eventName, handler) {
+    let handlers = this._eventHandlers?.[eventName];
+    if (!handlers) return;
+    for (let i = 0; i < handlers.length; i++) {
+      if (handlers[i] === handler) {
+        handlers.splice(i--, 1);
+      }
+    }
+  },
+
+  /**
+   * Створити подію з вказаною назвою та даними
+   *  this.trigger('select', data1, data2);
+   */
+  trigger(eventName, ...args) {
+    if (!this._eventHandlers?.[eventName]) {
+      return; // немає обробників для події з цією назвою
+    }
+
+    // виклик обробників
+    this._eventHandlers[eventName].forEach(handler => handler.apply(this, args));
+  }
+};
