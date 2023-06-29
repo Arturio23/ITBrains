9564e5cce1014a82af2b4fc41c129d0a63c3c6a8
imag <daysartur@gmail.com>
2023-06-29 14:16:25 +0300

    ss

---
+++ JS/сlass38.js
@@ -0,0 +1,18 @@
+class MyError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = this.constructor.name;
+  }
+}
+
+class ValidationError extends MyError { }
+
+class PropertyRequiredError extends ValidationError {
+  constructor(property) {
+    super("No property: " + property);
+    this.property = property;
+  }
+}
+
+// правильна name
+alert( new PropertyRequiredError("field").name ); // PropertyRequiredError
