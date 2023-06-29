6a67a59f861e398e3ed8e216bc19cddbc67561ff
imag <daysartur@gmail.com>
2023-06-29 13:49:46 +0300

    ss

---
+++ JS/сlass35.js
@@ -0,0 +1,18 @@
+class ValidationError extends Error {
+  constructor(message) {
+    super(message); // (1)
+    this.name = "ValidationError"; // (2)
+  }
+}
+
+function test() {
+  throw new ValidationError("Упс!");
+}
+
+try {
+  test();
+} catch(err) {
+  alert(err.message); // Упс!
+  alert(err.name); // ValidationError
+  alert(err.stack); // список вкладених викликів з номерами рядків для кожного
+}
