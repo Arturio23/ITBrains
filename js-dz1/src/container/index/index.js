b15fe6f84808abf58c2d74c5a02c3341bf251655
imag <daysartur@gmail.com>
2023-08-06 08:20:46 +0300

    222

--- js-dz1/src/container/index/index.js
+++ js-dz1/src/container/index/index.js
@@ -1,120 +1,90 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const todoList = document.getElementById('todo-list')
+    const todoInput = document.getElementById('todo-input')
+    const addButton = document.getElementById('add-button')
+
+    // Get tasks from local storage or initialize an empty array
+    let tasks =
+        JSON.parse(localStorage.getItem('tasks')) || []
+
+    function saveTasksToLocalStorage() {
+        localStorage.setItem('tasks', JSON.stringify(tasks))
+    }
+
+    function renderTasks() {
+        todoList.innerHTML = ''
+        tasks.forEach((task, index) => {
+            const li = document.createElement('li')
+            li.classList.add('todo-app__task')
+            li.innerHTML = `
+                <div class="todo-app__task-text ${
+                  task.completed
+                    ? 'todo-app__task-text--completed'
+                    : ''
+                }">${task.text}</div>
+                <button class="todo-app__delete-button">Delete</button>
+                <button class="todo-app__complete-button">${
+                  task.completed ? 's' : 's'
+                }</button>
+            `
+
+            // Delete button event handler
+            li.querySelector(
+                '.todo-app__delete-button',
+            ).addEventListener('click', () => {
+                tasks.splice(index, 1)
+                saveTasksToLocalStorage()
+                renderTasks()
+            })
+
+            // Complete button event handler
+            li.querySelector(
+                '.todo-app__complete-button',
+            ).addEventListener('click', () => {
+                task.completed = !task.completed
+                saveTasksToLocalStorage()
+                renderTasks()
+            })
+
+            todoList.appendChild(li)
+        })
+    }
+
+    // Add button event handler
+    addButton.addEventListener('click', () => {
+        const taskText = todoInput.value.trim()
+        if (taskText !== '') {
+            tasks.push({
+                text: taskText,
+                completed: false,
+            })
+            todoInput.value = ''
+            saveTasksToLocalStorage()
+            renderTasks()
+        }
+    })
+
+    renderTasks()
+})
 document.addEventListener('DOMContentLoaded', function () {
-    const taskInput = document.getElementById('taskInput')
-    const addTaskBtn = document.getElementById('addTaskBtn')
-    const taskList = document.getElementById('taskList')
-    const deleteButton =
-        document.getElementById('deleteButton')
     const scrollToTopBtn = document.querySelector(
         '.scroll-to-top',
     )
 
-    // Відслідковуємо подію прокрутки сторінки
+    // Show/hide the "scroll-to-top" button on scroll
     window.addEventListener('scroll', () => {
         if (window.pageYOffset > 200) {
-            // З'являємо кнопку "up to scroll" при прокрутці на відстань 200px від верху
             scrollToTopBtn.style.display = 'block'
         } else {
             scrollToTopBtn.style.display = 'none'
         }
     })
 
-    function loadTasks() {
-        const tasks =
-            JSON.parse(localStorage.getItem('tasks')) || []
-        tasks.forEach((task) => {
-            createTaskItem(task)
-        })
-    }
-
-    function addTask() {
-        const taskText = taskInput.value.trim()
-        if (taskText !== '') {
-            createTaskItem(taskText)
-            taskInput.value = ''
-            saveTasksToLocalStorage()
-        }
-    }
-
-    function createTaskItem(text) {
-        const taskItem = document.createElement('li')
-        taskItem.classList.add('todo-app__task-item')
-
-        const taskTextElement = document.createElement('span')
-        taskTextElement.textContent = text
-
-        const deleteButton = document.createElement('button')
-        deleteButton.classList.add('todo-app__delete-button')
-
-        const trashIcon = document.createElement('span')
-        trashIcon.classList.add('todo-app__trash-icon')
-        deleteButton.appendChild(trashIcon)
-
-        taskItem.appendChild(taskTextElement)
-        taskItem.appendChild(deleteButton)
-
-        taskList.appendChild(taskItem)
-
-        deleteButton.addEventListener('click', () => {
-            deleteTask(taskItem)
-        })
-    }
-
-    function deleteTask(taskItem) {
-        taskList.removeChild(taskItem)
-        saveTasksToLocalStorage()
-    }
-
-    function saveTasksToLocalStorage() {
-        const tasks = Array.from(taskList.children).map(
-            (taskItem) => taskItem.firstChild.textContent,
-        )
-        localStorage.setItem('tasks', JSON.stringify(tasks))
-    }
-
-    addTaskBtn.addEventListener('click', addTask)
-    taskInput.addEventListener('keypress', function (event) {
-        if (event.key === 'Enter') {
-            addTask()
-        }
-    })
-
-    deleteButton.addEventListener('click', () => {
-        const allTasks = document.querySelectorAll(
-            '.todo-app__task-item',
-        )
-        allTasks.forEach((taskItem) => {
-            if (taskItem.classList.contains('selected')) {
-                deleteTask(taskItem)
-            }
-        })
-    })
-
-    taskList.addEventListener('click', (event) => {
-        const clickedElement = event.target
-        if (
-            clickedElement.classList.contains(
-                'todo-app__delete-button',
-            )
-        ) {
-            const taskItem = clickedElement.parentNode
-            deleteTask(taskItem)
-        } else if (
-            clickedElement.classList.contains(
-                'todo-app__task-item',
-            )
-        ) {
-            clickedElement.classList.toggle('selected')
-        }
-    })
-
-    // Логіка для кнопки "up to scroll"
+    // Scroll to top when the button is clicked
     scrollToTopBtn.addEventListener('click', () => {
         window.scrollTo({
             top: 0,
-            behavior: 'smooth'
-        });
-
-    });
-
-    loadTasks()
+            behavior: 'smooth',
+        })
+    })
 })
